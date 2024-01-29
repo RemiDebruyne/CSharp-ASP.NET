0588dc68136175717181b40986cd697e8fc5b5fe
remiD <remidebruyne@hotmail.com>
2024-01-29 20:23:52 +0100

    initial commit

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciceContact.Controllers
+{
+    public class ContactsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<string> contacts = new List<string>()
+            {
+                "Manu",
+                "Jean",
+                "Meluche",
+            };
+
+            ViewData["contacts"] = contacts;
+
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            return View();
+
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
